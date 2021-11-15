4d2b7197f0ca4c50031ca0dbaa4541518c582087
KirillPogranichny <kirillpogranichny@gmail.com>
2021-11-15 17:29:59 +1000

    Homework 2 done

---
+++ app/src/main/java/ru/fefu/activitytracker/screens/activities/MainScreenActivity.kt
@@ -0,0 +1,53 @@
+package ru.fefu.activitytracker.screens.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import ru.fefu.activitytracker.R
+import ru.fefu.activitytracker.screens.fragments.FragmentAction
+import ru.fefu.activitytracker.screens.fragments.FragmentProfile
+
+
+class MainScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().apply {
+                add(R.id.fragmentContainer, FragmentAction(), "Action")
+                commit()
+            }
+        }
+
+        val btnNav = findViewById<BottomNavigationView>(R.id.botNavBar)
+        btnNav.setOnItemSelectedListener {
+            val activity = supportFragmentManager.findFragmentByTag("Action")
+            val profile = supportFragmentManager.findFragmentByTag("Profile")
+
+            if (it.itemId == R.id.action_activity) {
+                if (activity !== null) {
+                    supportFragmentManager.beginTransaction().show(activity).commit()
+                }
+                if (profile !== null) {
+                    supportFragmentManager.beginTransaction().hide(profile).commit()
+                }
+            } else if (it.itemId == R.id.action_profile) {
+
+                if (activity !== null) {
+                    supportFragmentManager.beginTransaction().hide(activity).commit()
+                }
+                if (profile !== null) {
+                    supportFragmentManager.beginTransaction().show(profile).commit()
+
+                } else {
+                    supportFragmentManager.beginTransaction().apply {
+                        add(R.id.fragmentContainer, FragmentProfile(), "Profile")
+                        commit()
+                    }
+                }
+            }
+            true
+        }
+    }
+}
---
+++ app/src/main/java/ru/fefu/activitytracker/screens/fragments/FragmentProfile.kt
@@ -0,0 +1,24 @@
+package ru.fefu.activitytracker.screens.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.fefu.activitytracker.databinding.FragmentProfileBinding
+
+
+class FragmentProfile : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentProfileBinding.inflate(inflater)
+        return binding.root
+    }
+
+    companion object {
+        fun newFragment() = FragmentProfile()
+    }
+}
